6b2dd50bce0792255941fb519aeecd53d17d12bf
Filip Chalupa <chalupafilip@gmail.com>
2013-08-14 22:44:22 +0200

    Wrong IP

--- public/media/js/basic.js
+++ public/media/js/basic.js
@@ -139,7 +139,7 @@ $(function () {
 		$messagesRoom.children('.'+message).addClass('show');
 	}
 	function connect() {
-		socket = io.connect("http://192.168.1.230", {port: 80, transports: ["websocket"]});
+		socket = io.connect("http://192.168.1.7", {port: 80, transports: ["websocket"]});
 		setEventHandlers();
 	};
 	var setEventHandlers = function() {
