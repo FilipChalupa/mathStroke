3e9a5e75e26c27e5714b07c1d7b327fd093f9ccf
Filip Chalupa <chalupa.filip@gmail.com>
2015-03-02 15:36:00 +0100

    Openshift fix

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var http = require("http"),
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80;
 var websocket_port = server_port;
+if (process.env.OPENSHIFT_NODEJS_PORT) {
+    websocket_port = 8000;
+}
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var socket,
